e44752f5166901242f360b3a18f0f811283b824f
Guillermo Aguilar <gaguilar128@gmail.com>
2021-05-22 19:39:22 -0500

    implementMax

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const PORT = 5000
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const requests = [];
+
+app.use('/', async (req, res) => {
+	const { type } = req.body;
+
+	if (['compute_max', 'compute_min'].includes(type)) {
+		requests.push({ length: req.body.length, max: 0, index: 1 });
+		const requestId = requests.length - 1;
+
+		return res.json({
+			type: "compare",
+			left: 0,
+			right: 1,
+			request_id: requestId
+		})
+	}
+
+	if (type === 'comp_result') {
+		const value = requests[req.body.request_id]
+
+		if (req.body.answer) {
+			value.max = value.index;
+		}
+
+		value.index += 1
+
+		if (value.index === value.length) {
+			return res.json({
+				"type": "done",
+				"result": value.max,
+			})
+		}
+
+		res.json({
+			type: "compare",
+			left: value.max,
+			right: value.index,
+			request_id: req.body.request_id
+		})
+	}
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`)
+})
